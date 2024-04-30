a0dd109c14d0cda2621102b4f3839004ab9e4bcb
your-correct-username <takhminaacom@gmail.com>
2024-04-30 23:30:07 +0600

    commit kamila

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     name2 = models.CharField(max_length=100)
-    name2 = models.CharField(max_length=100)
+    name23 = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
